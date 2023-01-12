3b021d66acbda43c1c11c17b09ae7fb1bc273241
oguzhanascier <oguzhanascier20@gmail.com>
2023-01-12 14:36:08 +0300

    productcard style

---
+++ src/components/product/productCard.vue
@@ -0,0 +1,88 @@
+<template>
+    <div class="cards">
+        <div class="card">
+            <img src="../../assets/Image.png" alt="">
+            <div class="title">MEN'S BETTER THAN NAKED - JACKET</div>
+            <div class="price">99.99</div>
+            <div class="subText">1258 bids, 359 watchers.</div>
+            <div class="rating">
+                <div class="star">31</div>
+                <div class="watch">watch</div>
+            </div>
+        </div>
+        <div class="card">
+            <img src="../../assets/Image.png" alt="">
+            <div class="title">MEN'S BETTER THAN NAKED - JACKET</div>
+            <div class="price">99.99</div>
+            <div class="subText">1258 bids, 359 watchers.</div>
+            <div class="rating">
+                <div class="star">31</div>
+                <div class="watch">watch</div>
+            </div>
+        </div>
+        <div class="card">
+            <img src="../../assets/Image.png" alt="">
+            <div class="title">MEN'S BETTER THAN NAKED - JACKET</div>
+            <div class="price">99.99</div>
+            <div class="subText">1258 bids, 359 watchers.</div>
+            <div class="rating">
+                <div class="star">31</div>
+                <div class="watch">watch</div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+
+};
+</script>
+
+<style lang="scss">
+$line-height: 25px;
+
+.cards {
+    display: flex;
+    justify-content: space-around;
+    flex-wrap: wrap;
+}
+
+.card {
+    display: flex;
+    flex-direction: column;
+    width: 260px;
+    height: 473px;
+    padding: 16px;
+    font-family: 'Inter', sans-serif;
+}
+
+img {}
+
+.title {
+    font-size: 16px;
+    font-weight: regular;
+    line-height: $line-height;
+}
+
+.price {
+    font-size: 24px;
+    font-weight: bold;
+    line-height: 30px;
+
+}
+
+.subText {
+    font-size: 14px;
+    font-weight: 200;
+    line-height: $line-height;
+
+}
+
+.rating {
+    display: flex;
+    justify-content: space-between;
+    line-height: $line-height;
+
+}
+</style>
