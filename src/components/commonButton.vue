fa7d7faa682d60a109634ecf95542ee2d3bdce23
oguzhanascier <oguzhanascier20@gmail.com>
2023-01-12 14:36:48 +0300

    style

---
+++ src/components/commonButton.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="selectBox">
+        <span class="selectTop">SORT BY</span>
+        <select class="select">
+            <option value="">Decending Price</option>
+        </select>
+    </div>
+</template>
+
+<script>
+export default {
+
+};
+</script>
+
+<style lang="scss">
+.selectBox {
+    position: relative;
+
+}
+
+.selectTop{
+    font-size: 12px;
+    position: absolute;
+    top: -12px;
+    left: 5px;
+    padding-inline: 5px;
+    background: #ffff;
+    border: 1px solid #fff;
+}
+.select {
+    border: 1px solid #5A5B6A;
+    background: #ffff;
+    padding: 7px;
+}
+</style>
