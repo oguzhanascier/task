45bd980ba8c6c5301a4c313bab98ce27cf9fdcbe
oguzhanascier <oguzhanascier20@gmail.com>
2023-01-14 15:03:56 +0300

    get action

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,27 @@
   <router-view/>
 </template>
 
-<style lang="scss">
+<script>
+import { mapActions } from 'vuex';
+export default{
 
+ computed:{
+  ...mapActions(['getData'])
+
+ } ,
+ created(){
+ this.getData
+ }
+}
+
+</script>
+
+<style lang="scss">
+@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap');
+body{
+    padding: 0;
+    margin: 0;
+    font-family: 'Poppins', sans-serif;
+}
 </style>
