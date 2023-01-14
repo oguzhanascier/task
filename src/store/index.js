df250acc57ef684e245d015d2d6eea9d9d1f128e
oguzhanascier <oguzhanascier20@gmail.com>
2023-01-14 15:03:09 +0300

    try chat block

--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,28 @@
 import { createStore } from 'vuex'
+import axios from 'axios'
 
 export default createStore({
   state: {
+    data: []
   },
   getters: {
   },
   mutations: {
+    setData(state, data) {
+      state.data = data;
+    }
   },
   actions: {
+    async getData({ commit }) {
+      try {
+        const response = await axios('data/db.json');
+        commit('setData', response.data);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  
   },
-  modules: {
-  }
+modules: {
+}
 })
